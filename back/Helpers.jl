b254a5fc584d652dff11c1e17d8113599f3aec09
Евгений <evg.ivanov@corp.mail.ru>
2018-10-05 14:04:13 +0300

    fix

--- back/Helpers.jl
+++ back/Helpers.jl
@@ -3,7 +3,7 @@ module Helpers
 		# ничкго полезного, просто преобразует данные возвращаемые
 		# библиотечкой для интегрирования, в более удобный для дальнейшего использования вид
 		result_arr = Float64[]
-		for i in 1:length(u) - 1
+		for i in 1:length(u)
 			push!(result_arr, u[i][index])
 		end
 		return result_arr
@@ -11,12 +11,12 @@ module Helpers
 
 	function find_first_item(arr, item, last_index)
 		# функция находящая индекс первого элемента массива arr, большего чем item
-		for i in last_index:length(arr) - 1
+		for i in last_index:length(arr)
 			if arr[i] >= item
 				return i
 			end
 		end
-		return length(arr - 1)
+		return length(arr)
 	end
 
 	export convert_julia_chart_arr_in_js_arr, find_first_item
